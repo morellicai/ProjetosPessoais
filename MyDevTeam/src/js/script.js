9c68dccae61f929f701a3d8994189843ae55900f
Caio Morelli <caiomorelli.it@gmail.com>
2024-09-28 16:10:47 -0300

    Atualizacao no arquivo .js

--- MyDevTeam/src/js/script.js
+++ MyDevTeam/src/js/script.js
@@ -3,16 +3,18 @@ let lastScrollTop = 0;
 const header = document.getElementById('header');
 
 window.addEventListener('scroll', () => {
-    // Usando scrollY ao invés de pageYOffset
     const scrollTop = window.scrollY || document.documentElement.scrollTop;
 
     if (scrollTop > lastScrollTop) {
-        // Rolando para baixo, esconder o cabeçalho
         header.classList.add('hidden');
     } else {
-        // Rolando para cima, mostrar o cabeçalho
         header.classList.remove('hidden');
     }
-    
+
     lastScrollTop = scrollTop;
 });
+
+let button = document.getElementById("orcamento");
+button.addEventListener("click", () => {
+    button.style.backgroundColor = "black";
+})
