97cce82c68d3f53f11636004772197fee89484bb
Caio Morelli <caiomorelli.it@gmail.com>
2024-10-07 08:20:11 -0300

    atualizado alguns pontos no design que achei relevante, e adicionei interatividade nos botões que alteram o card dos serviços. ainda não sei o motivo de não funcionar o das imagens do time. Mas em breve o proximo commit ja estara funcionando

--- MyDevTeam/src/js/script.js
+++ MyDevTeam/src/js/script.js
@@ -17,4 +17,65 @@ window.addEventListener('scroll', () => {
 let button = document.getElementById("orcamento");
 button.addEventListener("click", () => {
     button.style.backgroundColor = "black";
+    button.style.color = "#4db8ff";
 })
+
+// Serviços
+const buttonServicos = document.getElementById("servicos");
+buttonServicos.addEventListener("click", () => {
+    const card = document.getElementById("card");
+})
+
+function card(event, type) {
+    event.preventDefault();
+
+    // Todos os IDs dos cartões
+    const cards = [
+        "card-desktop", 
+        "card-web", 
+        "card-mobile", 
+        "card-ecommerce", 
+        "card-design",
+        "card-suporte", 
+        "card-gestao"
+    ];
+
+    // Exibe o cartão correto com base no 'type' e oculta os outros
+    cards.forEach(cardId => {
+        const cardElement = document.getElementById(cardId);
+        cardElement.style.display = (cardId === `card-${type}`) ? "block" : "none";
+    });
+}
+
+// Adicionando os event listeners de forma genérica
+document.querySelectorAll('.list-services').forEach(button => {
+    button.addEventListener('click', function(event) {
+        const type = this.getAttribute('data-type');
+        card(event, type);
+    });
+});
+
+function devs(event, type){
+    event.preventDefault();
+    const cards = [
+        "card-eddie", 
+        "card-felipe", 
+        "card-elias", 
+        "card-dmitri",
+        "card-pedro",
+        "card-caio"
+    ];
+    cards.forEach(cardId => {
+        const cardElement = document.getElementById(cardId);
+        cardElement.style.display = (cardId === `card-${type}`) ? "flex" : "none";
+    });
+}
+
+// Adicionando os event listeners de forma genérica
+document.querySelectorAll('a[data-type]').forEach(button => {
+    button.addEventListener('click', function(event) {
+        const type = this.getAttribute('data-type');
+        console.log('Clique detectado no botão com tipo: ' + type);
+        card(event, type);
+    });
+});
